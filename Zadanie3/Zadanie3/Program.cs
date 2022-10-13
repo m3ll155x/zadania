98a3f21f27f7891a937609e6497e1d39228fea0b
m3ll155x <59847943+m3ll155x@users.noreply.github.com>
2022-10-13 10:07:29 +0200

    Add files via upload

---
+++ Zadanie3/Zadanie3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Zadanie3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę:");
+            int liczba = int.Parse(Console.ReadLine());
+
+
+            if (liczba % 2 == 0)
+            {
+                Console.WriteLine("Podana liczba jest parzysta.");
+            }
+            else
+            {
+                Console.WriteLine("Podana liczba jest nieparzysta.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
