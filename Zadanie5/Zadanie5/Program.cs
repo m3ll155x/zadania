2b372f3640521d3761d10fd71cee18992df643b7
m3ll155x <59847943+m3ll155x@users.noreply.github.com>
2022-10-13 09:58:59 +0200

    Add files via upload

---
+++ Zadanie5/Zadanie5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Zadanie5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę 1:");
+            int liczba1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Podaj liczbę 2:");
+            int liczba2 = Convert.ToInt32(Console.ReadLine());
+            var liczby = new int[] { liczba1, liczba2 };
+            double srednia = Queryable.Average(liczby.AsQueryable());
+            Console.WriteLine("Średnia to: " + srednia);
+            Console.ReadKey();
+        }
+    }
+}
