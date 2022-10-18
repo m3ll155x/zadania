2ba3af9061d6170914c72e94b1c7165af60eee48
Hania <59847943+m3ll155x@users.noreply.github.com>
2022-10-18 11:53:08 +0200

    Add files via upload

---
+++ zadanie7/zadanie7/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace zadanie7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, v;
+            int z = 0;
+            Console.Write("Podaj wartość x: ");
+            x = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Podaj wartość y: ");
+            y = Convert.ToInt32(Console.ReadLine());
+            
+            if (x <0 && y < 0)
+            {
+
+                v = x * y;
+                z = v;
+            }
+            else if (x==0||y == 0)
+                {
+
+                v = 10;
+                z = v;
+                }
+                else if (x>0&&y > 0)
+                {
+
+                v = x + y;
+                z = v;
+                }
+
+                Console.WriteLine("Wartość z wynosi:" + z);
+            
+            Console.ReadKey();
+        }
+    }
+}
