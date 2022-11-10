f2cb0d0f3d0ab156ebe255a7b67ea80ce7b1bc57
Hania <59847943+m3ll155x@users.noreply.github.com>
2022-11-10 12:59:54 +0100

    Add files via upload

---
+++ zad15/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace zad15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int p;
+            int litera;
+            Console.Write("Podaj przesuniecie: ");
+            p = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Zacznij wpisywać szyfr (q aby zakończyć): ");
+
+            while ((litera = Console.ReadKey(true).KeyChar) != 'q')
+            {
+                if (litera >= 'a' && litera <= 'z') Console.Write(Convert.ToChar((litera - 'a' + p) % 26 + 'a'));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
