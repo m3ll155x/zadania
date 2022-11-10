63c84ac1f1c1ece761e33e4ef79199f96c49b4a7
Hania <59847943+m3ll155x@users.noreply.github.com>
2022-11-10 19:01:22 +0100

    Add files via upload

---
+++ sumailoczyn/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace sumailoczyn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var suma = 0;
+            var iloczyn = 1;
+
+            int n, przezCoPodzielna, kolejnaPodzielna;
+
+            Console.Write("Podaj n\n> ");
+            var nstring = Console.ReadLine();
+            n = Int32.Parse(nstring);
+
+            Console.Write("\nPodaj k\n> ");
+            var kstring = Console.ReadLine();
+            przezCoPodzielna = Int32.Parse(kstring);
+
+            kolejnaPodzielna = przezCoPodzielna;
+
+            foreach (var i in Enumerable.Range(1, n))
+            {
+                suma += kolejnaPodzielna;
+                iloczyn = iloczyn * kolejnaPodzielna;
+                kolejnaPodzielna += przezCoPodzielna;
+            }
+
+            Console.WriteLine("\nsuma: {0}\niloczyn: {1}", suma, iloczyn);
+            Console.ReadKey();
+        }
+
+    }
+}
