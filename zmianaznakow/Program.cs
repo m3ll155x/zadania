ae6f3c74933383b41e2b03c2207c26c06a4e1d84
Hania <59847943+m3ll155x@users.noreply.github.com>
2022-11-15 12:10:02 +0100

    Add files via upload

---
+++ zmianaznakow/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace zmianaznakow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj slowo:");
+            string slowo = Console.ReadLine();
+            Console.WriteLine(zamiana(slowo));
+            Console.ReadKey();
+        }
+        public static string zamiana(string input)
+        {
+            return input.Length > 1
+                ? input.Substring(input.Length - 1) + input.Substring(1, input.Length - 2) + input.Substring(0, 1) : input;
+        }
+        
+    }
+}
