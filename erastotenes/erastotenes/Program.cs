c17a8fa3e937bb79f65061622f6d8847a6da5acd
Hania <59847943+m3ll155x@users.noreply.github.com>
2022-11-15 16:10:57 +0100

    Add files via upload

---
+++ erastotenes/erastotenes/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace erastotenes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MAX = 1000000;
+
+
+            bool[] lpierwsze = new bool[MAX + 1];
+            for (int i = 0; i < lpierwsze.Length; i++)
+            {
+                lpierwsze[i] = true;
+            }
+            for (int i = 2; i < Math.Sqrt(MAX) + 1; i++)
+            {
+                if (lpierwsze[i - 1])
+                {
+                    for (int j = (int)Math.Pow(i, 2); j <= MAX; j += i)
+                    {
+                        lpierwsze[j - 1] = false;
+                    }
+                }
+            }
+            int count = 0;
+            for (int i = 2; i < lpierwsze.Length; i++)
+            {
+                if (lpierwsze[i - 1])
+                {
+                    Console.WriteLine(i);
+                    count++;
+                }
+            }
+            Console.WriteLine($"Istnieje {count} liczb pierwszych do {MAX}");
+            Console.ReadKey();
+        }
+    }
+}
