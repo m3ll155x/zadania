6ac4ea53e87bb8a8a8091975734cae7e2fb8b894
Hania <59847943+m3ll155x@users.noreply.github.com>
2022-11-15 12:10:38 +0100

    Add files via upload

---
+++ odwroceniezdania/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace odwroceniezdania
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wprowadz zdanie:");
+            string zdanie = Console.ReadLine();
+            string result = "";
+            List<string> listaslow = new List<string>();
+            string[] slowa = zdanie.Split(new[] {" "}, StringSplitOptions.None);
+            for (int i = slowa.Length - 1; i >= 0; i--)
+            {
+                result += slowa[i] + " ";
+            }
+            listaslow.Add(result);
+            foreach (String s in listaslow)
+            {
+
+                Console.WriteLine("\nOdwrocone zdanie: " + s);
+            }
+            Console.ReadKey();
+        }
+      
+    }
+}
