d15e5585e1b9f19a07eafb6865c3e82f4637aa8c
Hania <59847943+m3ll155x@users.noreply.github.com>
2022-11-15 16:38:31 +0100

    Add files via upload

---
+++ tabtrojkat/tabtrojkat/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace tabtrojkat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            piramida();
+            Console.ReadKey();
+        }
+        static void piramida()
+        {
+            int[] arr = Enumerable.Range(1, 100).ToArray();
+            int n,m;
+            n = 14;
+            int k;
+            k = 100;
+            m = 8;
+            for (int i = 0; i < n; i++)
+            {
+
+                for (int j = 1; j <= i; j++)
+                {
+                    k--;
+                    Console.Write(arr[k] + " ");
+                }
+                Console.Write("\n");
+            }
+            for (int i = 0; i < 9; i++)
+            {
+                Console.Write(arr[m] + " ");
+                m--;
+            }
+
+        }
+    }
+}
